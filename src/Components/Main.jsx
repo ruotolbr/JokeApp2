10e76d993733dd54f6fef0a47cc7ab8eae51b101
ruotolbr1 <ruotolbr@gmail.com>
2021-01-15 12:04:38 -0500

    dummy commit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import Home from './Home'
 import Categories from './Categories'
-
+//dummy commit
 function Main(props) {
     return (
         <div className="main">
             
             <Route exact path="/ComponentA" component={Home} />
             <Route exact path="/ComponentB" component={Categories} />
-            
+    
         </div>
     );
 }
