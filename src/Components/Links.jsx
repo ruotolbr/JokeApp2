8f4331533d230d0eb4566d380214f6e88f3afaf2
ruotolbr1 <ruotolbr@gmail.com>
2021-01-21 11:58:37 -0500

    dummy commit

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -6,12 +6,12 @@ function Links(props) {
             <h1>Need More Jokes?</h1>
             <p>Check out the funny sites below: </p>
             <ul>
-                <li><a href="http://www.funnyshortjokes.com">Funny Short Jokes</a></li>
-                <li><a href="https://www.funny-jokes-quotes-sayings.com/">Funny-Jokes-Quotes-Sayings</a></li>
-                <li><a href="https://www.thelaughline.com/">The Laughline</a></li>
-                <li><a href="https://www.super-funny.com/">Super-Funny</a></li>
-                <li><a href="https://www.laffgaff.com/">LaffGaff</a></li>
-                <li><a href="http://www.laughfactory.com/jokes/latest-jokes">Laugh Factory</a></li>
+                <li className="jokelink"><a className="link" href="http://www.funnyshortjokes.com">Funny Short Jokes</a></li>
+                <li className="jokelink"><a className="link" href="https://www.funny-jokes-quotes-sayings.com/">Funny-Jokes-Quotes-Sayings</a></li>
+                <li className="jokelink"><a className="link" href="https://www.thelaughline.com/">The Laughline</a></li>
+                <li className="jokelink"><a className="link" href="https://www.super-funny.com/">Super-Funny</a></li>
+                <li className="jokelink"><a className="link" href="https://www.laffgaff.com/">LaffGaff</a></li>
+                <li className="jokelink"><a className="link" href="http://www.laughfactory.com/jokes/latest-jokes">Laugh Factory</a></li>
             </ul>
         </div>
     );
